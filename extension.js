55c36ef692ec095bfc4831c19a2e493c4ee7bd69
yupeng <1196194875@qq.com>
2018-09-30 15:12:04 +0800

    显示当前时间

--- extension.js
+++ extension.js
@@ -6,51 +6,58 @@ const vscode = require('vscode');
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "codingTime" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    // let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-    //     // The code you place here will be executed every time your command is executed
-
-    //     // Display a message box to the user
-    //     vscode.window.showInformationMessage('Hello World!');
-    // });
-
-    // context.subscriptions.push(disposable);
+    let currentPanel = vscode.WebviewPanel | undefined;
 
 
-    context.subscriptions.push(vscode.commands.registerCommand('codingTime.show',function () {
-        const panel = vscode.window.createWebviewPanel(
-            'codingTime',
-            '代码时间',
-            vscode.ViewColumn.One,
-            {}
-        );
+    context.subscriptions.push(vscode.commands.registerCommand('codingTime.show', function () {
+        const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+        if (currentPanel) {
+            currentPanel.reveal(columnToShowIn);
+        } else {
 
-        // panel.webview.html  = getWebviewContent();
+            currentPanel = vscode.window.createWebviewPanel(
+                'codingTime',
+                '代码时间',
+                vscode.ViewColumn.One, {}
+            );
+
+
+            const updateWebview = () => {
+                let now = new Date();
+                let time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+                currentPanel.webview.html = getWebviewContent(time);
+            }
+
+            // Set initial content
+            updateWebview();
+
+            // And schedule updates to the content every second
+            const interval = setInterval(updateWebview, 1000);
+
+            currentPanel.onDidDispose(() => {
+                // When the panel is closed, cancel any future updates to the webview content
+                clearInterval(interval);
+            }, null, context.subscriptions)
+        }
     }))
 }
-// function getWebviewContent() {
-//     return `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Coding Time</title>
-// </head>
-// <body>
-//     <div>这是一个内容</div>
-// </body>
-// </html>`
-// }
+
+function getWebviewContent(time) {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coding Time</title>
+</head>
+<body>
+    <div>这是一个内容:${time}</div>
+</body>
+</html>`
+}
 
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
