d0e895ba950cb6b60726945234c0ba5c85dfa050
yupeng <1196194875@qq.com>
2018-09-30 14:31:08 +0800

    temp

--- extension.js
+++ extension.js
@@ -13,15 +13,41 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    // let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    //     // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    //     // Display a message box to the user
+    //     vscode.window.showInformationMessage('Hello World!');
+    // });
 
-    context.subscriptions.push(disposable);
+    // context.subscriptions.push(disposable);
+
+
+    context.subscriptions.push(vscode.commands.registerCommand('codingTime.show',function () {
+        const panel = vscode.window.createWebviewPanel(
+            'codingTime',
+            '代码时间',
+            vscode.ViewColumn.One,
+            {}
+        );
+
+        // panel.webview.html  = getWebviewContent();
+    }))
 }
+// function getWebviewContent() {
+//     return `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Coding Time</title>
+// </head>
+// <body>
+//     <div>这是一个内容</div>
+// </body>
+// </html>`
+// }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
