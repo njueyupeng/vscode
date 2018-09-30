5864914264472b7d642fc13f45c6e9638ee47466
yupeng <1196194875@qq.com>
2018-09-30 17:47:23 +0800

    获取当前编辑的代码和时间

--- extension.js
+++ extension.js
@@ -2,11 +2,19 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const child_process = require('child_process');
+
+
+let lastFile = '';
+let lastHeartbeat = 0;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-    let currentPanel = vscode.WebviewPanel | undefined;
+    let currentPanel = vscode.WebviewPanel || undefined;
 
 
     context.subscriptions.push(vscode.commands.registerCommand('codingTime.show', function () {
@@ -18,7 +26,9 @@ function activate(context) {
             currentPanel = vscode.window.createWebviewPanel(
                 'codingTime',
                 '代码时间',
-                vscode.ViewColumn.One, {}
+                vscode.ViewColumn.One, {
+                    enableScripts: true
+                }
             );
 
 
@@ -43,6 +53,8 @@ function activate(context) {
 }
 
 function getWebviewContent(time) {
+    // const scriptPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'media', 'main.js'));
+    // const scriptUri = scriptPathOnDisk.with({ scheme: 'vscode-resource' });
     return `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -52,6 +64,15 @@ function getWebviewContent(time) {
 </head>
 <body>
     <div>这是一个内容:${time}</div>
+    <h1 id="lines-of-code-counter">0</h1>
+    <script>
+        const counter = document.getElementById('lines-of-code-counter');
+
+        let count = 0;
+        setInterval(() => {
+            counter.textContent = count++;
+        }, 100);
+    </script>
 </body>
 </html>`
 }
@@ -60,4 +81,65 @@ exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
+
+function getCodingTimeHome() {
+
+}
+
+function onChange() {
+    onEvent(true);
+}
+
+function onSave() {
+    onEvent(false);
+}
+
+function onEvent(isWrite) {
+    let editor = vscode.window.activeTextEditor;
+    if (editor) {
+        let doc = editor.document;
+        if (doc) {
+            let file = doc.fileName;
+            if (file) {
+                let time = Date.now();
+                if (isWrite || enoughTimePassed(time) || lastFile !== file) {
+                    // this.sendHeartbeat(file, isWrite);
+                    console.log(file);
+                    let project = getProjectName(file);
+                    console.log(project);
+                    lastFile = file;
+                    lastHeartbeat = time;
+                }
+            }
+        }
+    }
+}
+
+function enoughTimePassed(time) {
+    return lastHeartbeat + 120000 < time;
+}
+
+function getProjectName(file) {
+    let uri = vscode.Uri.file(file);
+    let workspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
+    if (vscode.workspace && workspaceFolder) {
+        try {
+            return workspaceFolder.name;
+        } catch (e) {}
+    }
+    return null;
+}
+function init() {
+    let statusBar = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left
+    );
+
+    statusBar.text = '$(clock)';
+    statusBar.tooltip = 'CodingTime is running...';
+    statusBar.show();
+    vscode.window.onDidChangeTextEditorSelection(onChange);
+    vscode.window.onDidChangeActiveTextEditor(onChange);
+    vscode.workspace.onDidSaveTextDocument(onSave);
+}
+init();
 exports.deactivate = deactivate;
